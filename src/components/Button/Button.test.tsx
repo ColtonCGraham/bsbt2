eb6b89d8a6724fd38daac79fa8a1228e591c1649
ColtonCGraham <coltoncgraham@hotmail.com>
2023-06-08 09:55:42 -0500

    Input added

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
-
+import { render, screen } from "@testing-library/react";
 import Button from "./Button";
+import "@testing-library/jest-dom/extend-expect";
 
 describe("Button", () => {
   test("renders the Button component", () => {
-    render(<Button label="Hello world!" />);
+    render(<Button text="Hello world!" />);
+  });
+});
+
+
+//colors are changed in styled components on property changes
+//tests are used to test logic not styles so checking for disabled instead of style change...
+describe("Button", () => {
+  test("is disabled", () => {
+    render(<Button text="Disabled" disabled={true} />);
+    const b =screen.getByText(/Disabled/i).closest('button');
+    expect(b).toBeDisabled();
   });
 });
